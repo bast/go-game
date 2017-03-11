3a33813315d752905ca567735a624b72f761f7f5
Radovan Bast <bast@users.noreply.github.com>
2017-03-11 11:55:37 +0100

    replace recursive group detection by todo stack

--- data_structures.go
+++ data_structures.go
@@ -63,6 +63,12 @@ func FormatMove(move Move) string {
 	return fmt.Sprintf("%c%d%c", letter, number, stoneColor)
 }
 
+type Group struct {
+	Color
+	points    map[Point]bool
+	liberties map[Point]bool
+}
+
 // The board is just a cache of moves. We can recompute the board from
 // the moves.
 type Game struct {
@@ -153,71 +159,62 @@ func (game *Game) findNeighbors(point Point) []Point {
 	return neighbors
 }
 
-// a variation of the https://en.wikipedia.org/wiki/Flood_fill algorithm
-func (game *Game) find_group(group []Point, point Point, color Color, liberties []Point, board_visited []bool) ([]Point, []Point, []bool) {
-
-	if game.Board[point] == 0 {
-		// this point is empty, therefore it is a liberty
-
-		// figure out whether we already know about this liberty
-		for _, l := range liberties {
-			if l == point {
-				// this liberty is already known
-				// do not append it and return
-				return group, liberties, board_visited
-			}
-		}
-		// this is a new libery so we add it to known liberties
-		liberties = append(liberties, point)
-		return group, liberties, board_visited
-	}
-
-	if game.Board[point] != color {
-		// this point has different color, return
-		return group, liberties, board_visited
-	} else {
-		// we have the same color
-		if board_visited[point.Y*game.Width+point.X] {
-			// we have been here before so let us return
-			return group, liberties, board_visited
-		} else {
-			// first time we are here
-			board_visited[point.Y*game.Width+point.X] = true
-		}
-		// add the point to the group
-		group = append(group, point)
-		// then traverse all neighbors before returning
-		for _, neighbor := range game.findNeighbors(point) {
-			group, liberties, board_visited = game.find_group(group, neighbor, color, liberties, board_visited)
-		}
-		return group, liberties, board_visited
-	}
-}
-
 func (game *Game) findCapturedGroups() {
 
-	board_visited := make([]bool, game.Width*game.Height)
+	groups := []Group{}
+	grouped_points := make(map[Point]bool)
+
+	for y := 0; y < game.Height; y++ {
+		for x := 0; x < game.Width; x++ {
+			point := Point{x, y}
+			color := game.Board[Point{x, y}]
+			if color != 0 {
+				if !grouped_points[point] {
+					group := Group{
+						Color:     color,
+						points:    make(map[Point]bool),
+						liberties: make(map[Point]bool),
+					}
+
+					todo := []Point{}
+					todo = append(todo, point)
+
+					for {
+						if len(todo) == 0 {
+							// queue is empty, let us stop
+							break
+						}
+
+						// pop the last element
+						// see https://github.com/golang/go/wiki/SliceTricks
+						point, todo = todo[len(todo)-1], todo[:len(todo)-1]
+
+						if !grouped_points[point] {
+							_color := game.Board[point]
+							if _color == 0 {
+								group.liberties[point] = true
+							} else if _color == group.Color {
+								group.points[point] = true
+								grouped_points[point] = true
+								// extend the todo array by neighbors
+								todo = append(todo, game.findNeighbors(point)...)
+							}
+						}
+					}
+					groups = append(groups, group)
+
+				}
+			}
+		}
+	}
 
 	// for debugging
 	board_num_liberties := make([]int, game.Width*game.Height)
 
-	group := []Point{}
-	for y := 0; y < game.Height; y++ {
-		for x := 0; x < game.Width; x++ {
-			if game.Board[Point{x, y}] != 0 {
-				if !board_visited[y*game.Width+x] {
-					liberties := []Point{}
-					group, liberties, board_visited = game.find_group(
-						[]Point{},
-						Point{x, y},
-						game.Board[Point{x, y}],
-						liberties,
-						board_visited)
-					for _, point := range group {
-						board_num_liberties[point.Y*game.Width+point.X] = len(liberties)
-					}
-				}
-			}
+	for _, group := range groups {
+		num_liberties := len(group.liberties)
+		for point, _ := range group.points {
+			board_num_liberties[point.Y*game.Width+point.X] = num_liberties
 		}
 	}
 
