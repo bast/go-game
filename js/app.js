dd156ad14b650926ccec85e71e0606b94080dc29
Radovan Bast <bast@users.noreply.github.com>
2019-01-18 17:50:22 +0100

    find groups

--- js/app.js
+++ js/app.js
@@ -9,6 +9,30 @@ const BLACK = 1;
 const WHITE = 2;
 
 
+function _get_neighbors(position) {
+    var x = position[0];
+    var y = position[1];
+
+    return [[x - 1, y],
+            [x + 1, y],
+            [x, y - 1],
+            [x, y + 1]];
+}
+
+
+function _position_outside_board(position) {
+    var x = position[0];
+    var y = position[1];
+
+    if (x < 1) return true;
+    if (x > _num_columns) return true;
+    if (y < 1) return true;
+    if (y > _num_rows) return true;
+
+    return false;
+}
+
+
 function _get_random_int(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -34,6 +58,8 @@ var app = new Vue({
         color_current_move: BLACK,
         colors: _reset(_num_rows, _num_columns, '#d6b489'),
         board: _reset(_num_rows, _num_columns, EMPTY),
+        groups: _reset(_num_rows, _num_columns, 0),
+        liberties: [],
     },
     computed: {
         // this is a bit of a hack, will clean this up later
@@ -90,6 +116,58 @@ var app = new Vue({
                     var i = _get_random_int(0, 2);
                     this.board[[row, col]] = i;
                     this.colors[[row, col]] = this.color(i);
+                }
+            }
+        },
+        _visit_neighbor: function(neighbor, current_color, current_group) {
+            // skip if neighbor is outside
+            if (_position_outside_board(neighbor)) { return; }
+
+            // skip if we have already visited this stone
+            if (this.groups[neighbor] > 0) { return; }
+
+            // neighbor has different color, so skip
+            if (this.board[neighbor] != current_color) { return; }
+
+            // if neighbor empty, add to the liberties of this group
+            if (this.board[neighbor] == EMPTY) {
+                if (current_group in this.liberties) {
+                    this.liberties[current_group].push(neighbor);
+                } else {
+                    this.liberties[current_group] = [neighbor];
+                }
+                return;
+            }
+
+            // neighbor has same color, add it to this group
+            this.groups[neighbor] = current_group;
+
+            for (var _neighbor of _get_neighbors(neighbor)) {
+                this._visit_neighbor(_neighbor, current_color, current_group);
+            }
+        },
+        compute_groups: function() {
+            this.groups = _reset(_num_rows, _num_columns, 0);
+
+            var current_group = 1;
+            for (var row = 1; row <= _num_rows; row++) {
+                for (var col = 1; col <= _num_columns; col++) {
+                    var position = [col, row];
+
+                    // skip if empty
+                    if (this.board[position] == EMPTY) { continue; }
+
+                    // skip if we have already visited this stone
+                    if (this.groups[position] > 0) { continue; }
+
+                    var current_color = this.board[position];
+
+                    for (var neighbor of _get_neighbors(position)) {
+                        this._visit_neighbor(neighbor, current_color, current_group);
+                    }
+
+                    this.groups[position] = current_group;
+                    current_group++;
                 }
             }
         },
